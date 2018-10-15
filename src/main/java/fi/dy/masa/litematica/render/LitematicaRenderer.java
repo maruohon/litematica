ab6c88ae4db27539b09223e14dfda8055ff224c9
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2018-10-15 22:43:23 +0300

    Fix the block overlay sometimes rendering as all black

--- src/main/java/fi/dy/masa/litematica/render/LitematicaRenderer.java
+++ src/main/java/fi/dy/masa/litematica/render/LitematicaRenderer.java
@@ -212,7 +212,7 @@ public class LitematicaRenderer
             GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
             GlStateManager.glLineWidth((float) Configs.Visuals.SCHEMATIC_OVERLAY_OUTLINE_WIDTH.getDoubleValue());
             GlStateManager.color(1f, 1f, 1f, 1f);
-            //GlStateManager.disableDepth();
+            OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, 240, 240);
 
             if (Configs.Visuals.SCHEMATIC_OVERLAY_RENDER_THROUGH.getBooleanValue())
             {
