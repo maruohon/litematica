a3698c77a81a66c20ea65c0d581d6df54fab4f0d
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2020-02-20 19:55:47 +0200

    Clean up the Debug Hud Mixin. Fixes the flickering with Optifine.

--- src/main/java/fi/dy/masa/litematica/mixin/MixinDebugHud.java
+++ src/main/java/fi/dy/masa/litematica/mixin/MixinDebugHud.java
@@ -4,53 +4,31 @@ import java.util.List;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import net.minecraft.client.gui.DrawableHelper;
+import net.minecraft.client.gui.hud.DebugHud;
 import fi.dy.masa.litematica.render.LitematicaRenderer;
 import fi.dy.masa.litematica.render.schematic.WorldRendererSchematic;
 import fi.dy.masa.litematica.world.SchematicWorldHandler;
 import fi.dy.masa.litematica.world.WorldSchematic;
 import fi.dy.masa.malilib.gui.GuiBase;
-import net.minecraft.client.gui.DrawableHelper;
-import net.minecraft.client.gui.hud.DebugHud;
 
 @Mixin(DebugHud.class)
 public abstract class MixinDebugHud extends DrawableHelper
 {
-    private boolean captureNextCall = false;
-
-    @Inject(method = "renderLeftText()V", at = @At(value = "HEAD"))
-    private void onRenderDebugInfoLeft(CallbackInfo ci)
-    {
-        this.captureNextCall = true;
-    }
-
-    @Redirect(method = "renderLeftText()V", require = 0,
-              at = @At(value = "INVOKE", remap = false,
-                       target = "Ljava/util/List;size()I"))
-    private int getSize(List<String> list)
-    {
-        if (this.captureNextCall)
-        {
-            this.captureNextCall = false;
-            this.addInfoLines(list);
-        }
-
-        return list.size();
-    }
-
-    private void addInfoLines(List<String> list)
+    @Inject(method = "getLeftText", at = @At("RETURN"))
+    private void addDebugLines(CallbackInfoReturnable<List<String>> cir)
     {
         WorldSchematic world = SchematicWorldHandler.getSchematicWorld();
 
         if (world != null)
         {
+            List<String> list = cir.getReturnValue();
             String pre = GuiBase.TXT_GOLD;
             String rst = GuiBase.TXT_RST;
 
             WorldRendererSchematic renderer = LitematicaRenderer.getInstance().getWorldRenderer();
 
-            list.add("");
             list.add(String.format("%s[Litematica]%s %s", pre, rst, renderer.getDebugInfoRenders()));
 
             String str = String.format("E: %d TE: %d", world.getRegularEntityCount(), world.blockEntities.size());
