3f2dae8d2330abc84f049e69d0860312143a2152
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2020-03-19 23:46:41 +0200

    Update the renderer directly from any client world block changes

--- src/main/java/fi/dy/masa/litematica/mixin/MixinWorldClient.java
+++ src/main/java/fi/dy/masa/litematica/mixin/MixinWorldClient.java
@@ -1,38 +1,38 @@
 package fi.dy.masa.litematica.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import fi.dy.masa.litematica.config.Configs;
 import fi.dy.masa.litematica.schematic.verifier.SchematicVerifier;
 import fi.dy.masa.litematica.world.SchematicWorldRenderingNotifier;
-import net.minecraft.block.state.IBlockState;
-import net.minecraft.client.multiplayer.WorldClient;
-import net.minecraft.profiler.Profiler;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import net.minecraft.world.WorldProvider;
-import net.minecraft.world.storage.ISaveHandler;
-import net.minecraft.world.storage.WorldInfo;
 
-@Mixin(WorldClient.class)
-public abstract class MixinWorldClient extends World
+@Mixin(net.minecraft.client.multiplayer.WorldClient.class)
+public abstract class MixinWorldClient extends net.minecraft.world.World
 {
-    protected MixinWorldClient(ISaveHandler saveHandlerIn, WorldInfo info, WorldProvider providerIn, Profiler profilerIn, boolean client)
+    protected MixinWorldClient(
+            net.minecraft.world.storage.ISaveHandler saveHandlerIn,
+            net.minecraft.world.storage.WorldInfo info,
+            net.minecraft.world.WorldProvider providerIn,
+            net.minecraft.profiler.Profiler profilerIn, boolean client)
     {
         super(saveHandlerIn, info, providerIn, profilerIn, client);
     }
 
-    @Inject(method = "invalidateRegionAndSetBlock", at = @At("HEAD"))
-    private void onInvalidateRegionAndSetBlock(BlockPos pos, IBlockState state, CallbackInfoReturnable<Boolean> ci)
+    @Override
+    public boolean setBlockState(net.minecraft.util.math.BlockPos pos, net.minecraft.block.state.IBlockState newState, int flags)
     {
-        SchematicVerifier.markVerifierBlockChanges(pos);
+        boolean success = super.setBlockState(pos, newState, flags);
 
-        if (Configs.Visuals.ENABLE_RENDERING.getBooleanValue() &&
-            Configs.Visuals.ENABLE_SCHEMATIC_RENDERING.getBooleanValue())
+        if (success)
         {
-            SchematicWorldRenderingNotifier.markSchematicChunkForRenderUpdate(pos);
+            SchematicVerifier.markVerifierBlockChanges(pos);
+
+            if (Configs.Visuals.ENABLE_RENDERING.getBooleanValue() &&
+                Configs.Visuals.ENABLE_SCHEMATIC_RENDERING.getBooleanValue())
+            {
+                SchematicWorldRenderingNotifier.markSchematicChunkForRenderUpdate(pos);
+            }
         }
+
+        return success;
     }
 }
