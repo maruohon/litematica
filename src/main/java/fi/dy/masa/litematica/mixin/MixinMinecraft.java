f41f022ddfd0133d2c810eb34c995cc2a653ed9e
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2020-03-25 23:05:18 +0200

    Easy Place: Fix a missing check for the enabled option

--- src/main/java/fi/dy/masa/litematica/mixin/MixinMinecraft.java
+++ src/main/java/fi/dy/masa/litematica/mixin/MixinMinecraft.java
@@ -4,6 +4,8 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import fi.dy.masa.litematica.config.Configs;
+import fi.dy.masa.litematica.config.Hotkeys;
 import fi.dy.masa.litematica.data.DataManager;
 import fi.dy.masa.litematica.schematic.util.SchematicEditUtils;
 import fi.dy.masa.litematica.util.EasyPlaceUtils;
@@ -15,7 +17,11 @@ public abstract class MixinMinecraft
             target = "Lnet/minecraft/client/Minecraft;rightClickMouse()V"))
     private void onRightClickMouse(CallbackInfo ci)
     {
-        EasyPlaceUtils.setIsFirstClick(true);
+        if (Configs.Generic.EASY_PLACE_MODE.getBooleanValue() &&
+            Hotkeys.EASY_PLACE_ACTIVATION.getKeybind().isKeybindHeld())
+        {
+            EasyPlaceUtils.setIsFirstClick(true);
+        }
     }
 
     @Inject(method = "rightClickMouse", cancellable = true, at = @At(value = "FIELD", ordinal = 0,
@@ -31,7 +37,11 @@ public abstract class MixinMinecraft
     @Inject(method = "rightClickMouse", at = @At("TAIL"))
     private void onRightClickMouseTail(CallbackInfo ci)
     {
-        EasyPlaceUtils.onRightClickTail((net.minecraft.client.Minecraft)(Object) this);
+        if (Configs.Generic.EASY_PLACE_MODE.getBooleanValue() &&
+            Hotkeys.EASY_PLACE_ACTIVATION.getKeybind().isKeybindHeld())
+        {
+            EasyPlaceUtils.onRightClickTail((net.minecraft.client.Minecraft)(Object) this);
+        }
     }
 
     @Inject(method = "clickMouse", cancellable = true, at = @At(value = "FIELD", ordinal = 0,
