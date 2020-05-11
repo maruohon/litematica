f996e9eaefde1c7840847f599524e81fb248020c
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2020-05-11 21:16:42 +0300

    Fix Delete mode block updates, by preventing breakBlock() calls entirely
    This also removes the need for the neighbor notify cancellation in World

--- src/main/java/fi/dy/masa/litematica/mixin/MixinChunk.java
+++ src/main/java/fi/dy/masa/litematica/mixin/MixinChunk.java
@@ -3,17 +3,15 @@ package fi.dy.masa.litematica.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.Slice;
-import fi.dy.masa.litematica.util.WorldUtils;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.Chunk;
+import fi.dy.masa.litematica.util.WorldUtils;
 
 @Mixin(Chunk.class)
 public abstract class MixinChunk
 {
     @Redirect(method = "setBlockState",
-                slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/world/chunk/Chunk;generateSkylightMap()V")),
-                at = @At(value = "FIELD", target = "Lnet/minecraft/world/World;isRemote:Z"))
+              at = @At(value = "FIELD", target = "Lnet/minecraft/world/World;isRemote:Z"))
     private boolean redirectIsRemote(World world)
     {
         return WorldUtils.shouldPreventBlockUpdates() ? true : world.isRemote;
