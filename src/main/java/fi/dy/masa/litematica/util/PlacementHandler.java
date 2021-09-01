b4fdcc99d350a444caf45d5d23c7dd211a0d4b8f
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2021-09-02 00:12:20 +0300

    Easy Place: Implement a new, more generic placement protocol

--- src/main/java/fi/dy/masa/litematica/util/PlacementHandler.java
+++ src/main/java/fi/dy/masa/litematica/util/PlacementHandler.java
@@ -1,6 +1,10 @@
 package fi.dy.masa.litematica.util;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import javax.annotation.Nullable;
+import com.google.common.collect.ImmutableSet;
 import net.minecraft.block.BedBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
@@ -10,73 +14,102 @@ import net.minecraft.block.StairsBlock;
 import net.minecraft.block.TrapdoorBlock;
 import net.minecraft.block.enums.BlockHalf;
 import net.minecraft.block.enums.ComparatorMode;
+import net.minecraft.block.enums.SlabType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.state.property.Property;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
+import fi.dy.masa.litematica.Litematica;
+import fi.dy.masa.litematica.config.Configs;
 
 public class PlacementHandler
 {
+    public static final ImmutableSet<Property<?>> WHITELISTED_PROPERTIES = ImmutableSet.of(
+            // BooleanProperty:
+            // INVERTED - DaylightDetector
+            // OPEN - Barrel, Door, FenceGate, Trapdoor
+            // PERSISTENT - Leaves
+            Properties.INVERTED,
+            Properties.OPEN,
+            Properties.PERSISTENT,
+            // EnumProperty:
+            // AXIS - Pillar
+            // BLOCK_HALF - Stairs, Trapdoor
+            // CHEST_TYPE - Chest
+            // COMPARATOR_MODE - Comparator
+            // DOOR_HINGE - Door
+            // SLAB_TYPE - Slab - PARTIAL ONLY: TOP and BOTTOM, not DOUBLE
+            // STAIR_SHAPE - Stairs (needed to get the correct state, otherwise the player facing would be a factor)
+            // WALL_MOUNT_LOCATION - Button, Grindstone, Lever
+            Properties.AXIS,
+            Properties.BLOCK_HALF,
+            Properties.CHEST_TYPE,
+            Properties.COMPARATOR_MODE,
+            Properties.DOOR_HINGE,
+            Properties.SLAB_TYPE,
+            Properties.STAIR_SHAPE,
+            Properties.WALL_MOUNT_LOCATION,
+            // IntProperty:
+            // BITES - Cake
+            // DELAY - Repeater
+            // NOTE - NoteBlock
+            // ROTATION - Banner, Sign, Skull
+            Properties.BITES,
+            Properties.DELAY,
+            Properties.NOTE,
+            Properties.ROTATION
+    );
+
     @Nullable
     public static BlockState applyPlacementProtocolToPlacementState(BlockState state, UseContext context)
     {
-        Vec3d hitVec = context.getHitVec();
-        @Nullable DirectionProperty property = fi.dy.masa.malilib.util.BlockUtils.getFirstDirectionProperty(state);
-        int x = (int) (hitVec.x - (double) context.getPos().getX());
-        int rawFacingIndex = x & 0xF;
-
-        if (rawFacingIndex >= 2 && property != null)
+        if (Configs.Generic.EASY_PLACE_PROTOCOL_V3.getBooleanValue())
         {
-            Direction facingOrig = state.get(property);
-            Direction facing = facingOrig;
-            int decodedFacingIndex = ((x - 2) / 2);
+            return applyPlacementProtocolV3(state, context);
+        }
+        else
+        {
+            return applyPlacementProtocolV2(state, context);
+        }
+    }
 
-            if (decodedFacingIndex == 6) // the opposite of the normal facing requested
+    public static BlockState applyPlacementProtocolV2(BlockState state, UseContext context)
+    {
+        int protocolValue = (int) (context.getHitVec().x - (double) context.getPos().getX()) - 2;
+
+        if (protocolValue < 0)
+        {
+            return state;
+        }
+
+        @Nullable DirectionProperty property = fi.dy.masa.malilib.util.BlockUtils.getFirstDirectionProperty(state);
+
+        if (property != null)
+        {
+            state = applyDirectionProperty(state, context, property, protocolValue);
+
+            if (state == null)
             {
-                facing = facing.getOpposite();
-            }
-            else if (decodedFacingIndex >= 0 && decodedFacingIndex <= 5)
-            {
-                facing = Direction.byId(decodedFacingIndex);
-
-                if (property.getValues().contains(facing) == false)
-                {
-                    facing = context.getEntity().getHorizontalFacing().getOpposite();
-                }
-            }
-
-            //System.out.printf("plop facing: %s -> %s (raw: %d, dec: %d)\n", facingOrig, facing, rawFacingIndex, decodedFacingIndex);
-
-            if (facing != facingOrig && property.getValues().contains(facing))
-            {
-                if (state.getBlock() instanceof BedBlock)
-                {
-                    BlockPos headPos = context.pos.offset(facing);
-                    ItemPlacementContext ctx = context.getItemPlacementContext();
-
-                    if (context.getWorld().getBlockState(headPos).canReplace(ctx) == false)
-                    {
-                        return null;
-                    }
-                }
-
-                state = state.with(property, facing);
+                return null;
             }
         }
 
-        x &= 0xFFFFFFF0;
+        protocolValue &= 0xFFFFFFF0;
 
-        if (x >= 16)
+        if (protocolValue >= 16)
         {
             Block block = state.getBlock();
 
             if (block instanceof RepeaterBlock)
             {
-                Integer delay = (x / 16) + 1;
+                Integer delay = (protocolValue / 16) + 1;
 
                 if (RepeaterBlock.DELAY.getValues().contains(delay))
                 {
@@ -95,6 +128,122 @@ public class PlacementHandler
             {
                 state = state.with(StairsBlock.HALF, BlockHalf.TOP);
             }
+        }
+
+        return state;
+    }
+
+    public static <T extends Comparable<T>> BlockState applyPlacementProtocolV3(BlockState state, UseContext context)
+    {
+        int protocolValue = (int) (context.getHitVec().x - (double) context.getPos().getX()) - 2;
+        //System.out.printf("raw protocol value in: 0x%08X\n", protocolValue);
+
+        if (protocolValue < 0)
+        {
+            return state;
+        }
+
+        @Nullable DirectionProperty property = fi.dy.masa.malilib.util.BlockUtils.getFirstDirectionProperty(state);
+
+        // DirectionProperty - allow all except: VERTICAL_DIRECTION (PointedDripstone)
+        if (property != null)
+        {
+            //System.out.printf("applying: 0x%08X\n", protocolValue);
+            state = applyDirectionProperty(state, context, property, protocolValue);
+
+            if (state == null)
+            {
+                return null;
+            }
+
+            // Consume the bits used for the facing
+            protocolValue >>>= 3;
+        }
+
+        // Consume the lowest unused bit
+        protocolValue >>>= 1;
+
+        List<Property<?>> propList = new ArrayList<>(state.getBlock().getStateManager().getProperties());
+        propList.sort(Comparator.comparing(Property::getName));
+
+        try
+        {
+            for (Property<?> p : propList)
+            {
+                if ((p instanceof DirectionProperty) == false &&
+                    WHITELISTED_PROPERTIES.contains(p))
+                {
+                    @SuppressWarnings("unchecked")
+                    Property<T> prop = (Property<T>) p;
+                    List<T> list = new ArrayList<>(prop.getValues());
+                    list.sort(Comparable::compareTo);
+
+                    int requiredBits = MathHelper.log2(MathHelper.smallestEncompassingPowerOfTwo(list.size()));
+                    int bitMask = ~(0xFFFFFFFF << requiredBits);
+                    int valueIndex = protocolValue & bitMask;
+                    //System.out.printf("trying to apply valInd: %d, bits: %d, prot val: 0x%08X\n", valueIndex, requiredBits, protocolValue);
+
+                    if (valueIndex >= 0 && valueIndex < list.size())
+                    {
+                        T value = list.get(valueIndex);
+
+                        if (state.get(prop).equals(value) == false &&
+                            value != SlabType.DOUBLE) // don't allow duping slabs by forcing a double slab via the protocol
+                        {
+                            //System.out.printf("applying %s: %s\n", prop.getName(), value);
+                            state = state.with(prop, value);
+                        }
+
+                        protocolValue >>>= requiredBits;
+                    }
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Litematica.logger.warn("Exception trying to apply placement protocol value", e);
+        }
+
+        return state;
+    }
+
+    private static BlockState applyDirectionProperty(BlockState state, UseContext context,
+                                                     DirectionProperty property, int protocolValue)
+    {
+        Direction facingOrig = state.get(property);
+        Direction facing = facingOrig;
+        int decodedFacingIndex = (protocolValue & 0xF) >> 1;
+
+        if (decodedFacingIndex == 6) // the opposite of the normal facing requested
+        {
+            facing = facing.getOpposite();
+        }
+        else if (decodedFacingIndex >= 0 && decodedFacingIndex <= 5)
+        {
+            facing = Direction.byId(decodedFacingIndex);
+
+            if (property.getValues().contains(facing) == false)
+            {
+                facing = context.getEntity().getHorizontalFacing().getOpposite();
+            }
+        }
+
+        //System.out.printf("plop facing: %s -> %s (raw: %d, dec: %d)\n", facingOrig, facing, rawFacingIndex, decodedFacingIndex);
+
+        if (facing != facingOrig && property.getValues().contains(facing))
+        {
+            if (state.getBlock() instanceof BedBlock)
+            {
+                BlockPos headPos = context.pos.offset(facing);
+                ItemPlacementContext ctx = context.getItemPlacementContext();
+
+                if (context.getWorld().getBlockState(headPos).canReplace(ctx) == false)
+                {
+                    return null;
+                }
+            }
+
+            state = state.with(property, facing);
         }
 
         return state;
