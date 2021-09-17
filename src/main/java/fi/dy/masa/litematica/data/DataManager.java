hi5ilay/gitmatica

Improve the tool item config and detection (it allows NBT data now)

src/main/java/fi/dy/masa/litematica/data/DataManager.java
@@ -9,9 +9,12 @@ import javax.annotation.Nullable;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
+import com.mojang.brigadier.StringReader;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.StringNbtReader;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import fi.dy.masa.litematica.Litematica;
@@ -41,6 +44,7 @@ public class DataManager implements IDirectoryCache
 {
     private static final DataManager INSTANCE = new DataManager();
 
+    private static final Pattern PATTERN_ITEM_NBT = Pattern.compile("^(?<name>[a-z0-9\\._-]+:[a-z0-9\\._-]+)(?<nbt>\\{.*\\})$");
     private static final Pattern PATTERN_ITEM_BASE = Pattern.compile("^(?<name>(?:[a-z0-9\\._-]+:)[a-z0-9\\._-]+)$");
     private static final Map<String, File> LAST_DIRECTORIES = new HashMap<>();
 
@@ -437,9 +441,9 @@ public class DataManager implements IDirectoryCache
         return new File(dir, StringUtils.getStorageFileName(globalData, Reference.MOD_ID + "_", ".json", "default"));
     }
 
-    public static void setToolItem(String itemName)
+    public static void setToolItem(String itemNameIn)
     {
-        if (itemName.isEmpty() || itemName.equals("empty"))
+        if (itemNameIn.isEmpty() || itemNameIn.equals("empty"))
         {
             toolItem = ItemStack.EMPTY;
             return;
@@ -447,20 +451,35 @@ public class DataManager implements IDirectoryCache
 
         try
         {
-            Matcher matcher = PATTERN_ITEM_BASE.matcher(itemName);
+            Matcher matcherNbt = PATTERN_ITEM_NBT.matcher(itemNameIn);
+            Matcher matcherBase = PATTERN_ITEM_BASE.matcher(itemNameIn);
 
-            if (matcher.matches())
+            String itemName = null;
+            NbtCompound nbt = null;
+
+            if (matcherNbt.matches())
             {
-                Item item = Registry.ITEM.get(new Identifier(matcher.group("name")));
+                itemName = matcherNbt.group("name");
+                nbt = (new StringNbtReader(new StringReader(matcherNbt.group("nbt")))).parseCompound();
+            }
+            else if (matcherBase.matches())
+            {
+                itemName = matcherBase.group("name");
+            }
+
+            if (itemName != null)
+            {
+                Item item = Registry.ITEM.get(new Identifier(itemName));
 
                 if (item != null && item != Items.AIR)
                 {
                     toolItem = new ItemStack(item);
+                    toolItem.setNbt(nbt);
                     return;
                 }
             }
         }
-        catch (Exception e)
+        catch (Exception ignore)
         {
         }
 

src/main/java/fi/dy/masa/litematica/util/ItemUtils.java
@@ -1,6 +1,8 @@
 package fi.dy.masa.litematica.util;
 
+import java.util.HashSet;
 import java.util.IdentityHashMap;
+import java.util.Set;
 import net.minecraft.block.AbstractSkullBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
@@ -21,6 +23,34 @@ import net.minecraft.world.World;
 public class ItemUtils
 {
     private static final IdentityHashMap<BlockState, ItemStack> ITEMS_FOR_STATES = new IdentityHashMap<>();
+
+    public static boolean areTagsEqualIgnoreDamage(ItemStack stackReference, ItemStack stackToCheck)
+    {
+        NbtCompound tagReference = stackReference.getNbt();
+        NbtCompound tagToCheck = stackToCheck.getNbt();
+
+        if (tagReference != null && tagToCheck != null)
+        {
+            Set<String> keysReference = new HashSet<>(tagReference.getKeys());
+
+            for (String key : keysReference)
+            {
+                if (key.equals("Damage"))
+                {
+                    continue;
+                }
+
+                if (tagReference.get(key).equals(tagToCheck.get(key)) == false)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        return (tagReference == null) && (tagToCheck == null);
+    }
 
     public static ItemStack getItemForState(BlockState state)
     {

maruohon/litematica

src/main/java/fi/dy/masa/litematica/data/DataManager.java
@@ -9,9 +9,12 @@ import javax.annotation.Nullable;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
+import com.mojang.brigadier.StringReader;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.nbt.StringNbtReader;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import fi.dy.masa.litematica.Litematica;
@@ -41,6 +44,7 @@ public class DataManager implements IDirectoryCache
 {
     private static final DataManager INSTANCE = new DataManager();
 
+    private static final Pattern PATTERN_ITEM_NBT = Pattern.compile("^(?<name>[a-z0-9\\._-]+:[a-z0-9\\._-]+)(?<nbt>\\{.*\\})$");
     private static final Pattern PATTERN_ITEM_BASE = Pattern.compile("^(?<name>(?:[a-z0-9\\._-]+:)[a-z0-9\\._-]+)$");
     private static final Map<String, File> LAST_DIRECTORIES = new HashMap<>();
 
@@ -437,9 +441,9 @@ public class DataManager implements IDirectoryCache
         return new File(dir, StringUtils.getStorageFileName(globalData, Reference.MOD_ID + "_", ".json", "default"));
     }
 
-    public static void setToolItem(String itemName)
+    public static void setToolItem(String itemNameIn)
     {
-        if (itemName.isEmpty() || itemName.equals("empty"))
+        if (itemNameIn.isEmpty() || itemNameIn.equals("empty"))
         {
             toolItem = ItemStack.EMPTY;
             return;
@@ -447,20 +451,35 @@ public class DataManager implements IDirectoryCache
 
         try
         {
-            Matcher matcher = PATTERN_ITEM_BASE.matcher(itemName);
+            Matcher matcherNbt = PATTERN_ITEM_NBT.matcher(itemNameIn);
+            Matcher matcherBase = PATTERN_ITEM_BASE.matcher(itemNameIn);
 
-            if (matcher.matches())
+            String itemName = null;
+            NbtCompound nbt = null;
+
+            if (matcherNbt.matches())
             {
-                Item item = Registry.ITEM.get(new Identifier(matcher.group("name")));
+                itemName = matcherNbt.group("name");
+                nbt = (new StringNbtReader(new StringReader(matcherNbt.group("nbt")))).parseCompound();
+            }
+            else if (matcherBase.matches())
+            {
+                itemName = matcherBase.group("name");
+            }
+
+            if (itemName != null)
+            {
+                Item item = Registry.ITEM.get(new Identifier(itemName));
 
                 if (item != null && item != Items.AIR)
                 {
                     toolItem = new ItemStack(item);
+                    toolItem.setTag(nbt);
                     return;
                 }
             }
         }
-        catch (Exception e)
+        catch (Exception ignore)
         {
         }
 

src/main/java/fi/dy/masa/litematica/util/ItemUtils.java
@@ -1,6 +1,8 @@
 package fi.dy.masa.litematica.util;
 
+import java.util.HashSet;
 import java.util.IdentityHashMap;
+import java.util.Set;
 import net.minecraft.block.AbstractSkullBlock;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
@@ -21,6 +23,34 @@ import net.minecraft.world.World;
 public class ItemUtils
 {
     private static final IdentityHashMap<BlockState, ItemStack> ITEMS_FOR_STATES = new IdentityHashMap<>();
+
+    public static boolean areTagsEqualIgnoreDamage(ItemStack stackReference, ItemStack stackToCheck)
+    {
+        NbtCompound tagReference = stackReference.getTag();
+        NbtCompound tagToCheck = stackToCheck.getTag();
+
+        if (tagReference != null && tagToCheck != null)
+        {
+            Set<String> keysReference = new HashSet<>(tagReference.getKeys());
+
+            for (String key : keysReference)
+            {
+                if (key.equals("Damage"))
+                {
+                    continue;
+                }
+
+                if (tagReference.get(key).equals(tagToCheck.get(key)) == false)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        return (tagReference == null) && (tagToCheck == null);
+    }
 
     public static ItemStack getItemForState(BlockState state)
     {