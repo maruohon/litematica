df0c104ddc94665944b7c6ff7af7c23be9a739fc
AngelBottomless <35677394+aria1th@users.noreply.github.com>
2021-11-09 00:17:53 +0900

    Optimize Imports

--- src/main/java/fi/dy/masa/litematica/scheduler/ClientTickHandler.java
+++ src/main/java/fi/dy/masa/litematica/scheduler/ClientTickHandler.java
@@ -4,6 +4,7 @@ import net.minecraft.client.MinecraftClient;
 import fi.dy.masa.litematica.config.Configs;
 import fi.dy.masa.litematica.data.DataManager;
 import fi.dy.masa.litematica.selection.SelectionManager;
+import fi.dy.masa.litematica.util.RotationUtils;
 import fi.dy.masa.litematica.util.WorldUtils;
 import fi.dy.masa.malilib.interfaces.IClientTickHandler;
 import fi.dy.masa.malilib.util.EntityUtils;
