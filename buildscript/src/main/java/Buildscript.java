ca6227193d81b9c2c88f0b03c1807aefd703cfab
DragonEggBedrockBreaking <cuber3million@gmail.com>
2021-12-29 02:41:20 +0000

    Add maven publishing

--- buildscript/src/main/java/Buildscript.java
+++ buildscript/src/main/java/Buildscript.java
@@ -18,6 +18,11 @@ public class Buildscript extends FabricProject {
     }
 
     @Override
+    public String getMavenGroup() {
+        return "fi.dy.masa.litematica";
+    }
+
+    @Override
     public String getVersion() {
         return "0.9.0";
     }
