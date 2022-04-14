7967143bce09da98da0d1c9878c674b684be278f
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2022-04-14 20:57:04 +0300

    Add a bypass for the check in 1.18.2 that breaks accurate placement protocol
    This fixes the protocol handling in single player

---
+++ src/main/java/fi/dy/masa/litematica/mixin/MixinServerPlayNetworkHandler.java
@@ -0,0 +1,26 @@
+package fi.dy.masa.litematica.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import net.minecraft.server.network.ServerPlayNetworkHandler;
+import net.minecraft.util.math.Vec3d;
+import fi.dy.masa.litematica.config.Configs;
+
+@Mixin(value = ServerPlayNetworkHandler.class, priority = 1010)
+public class MixinServerPlayNetworkHandler
+{
+    @Redirect(method = "onPlayerInteractBlock", require = 0,
+              at = @At(value = "INVOKE",
+                       target = "Lnet/minecraft/util/math/Vec3d;subtract(Lnet/minecraft/util/math/Vec3d;)Lnet/minecraft/util/math/Vec3d;"))
+    private Vec3d tweakeroo_removeHitPosCheck(Vec3d hitVec, Vec3d blockCenter)
+    {
+        if (Configs.Generic.EASY_PLACE_MODE.getBooleanValue() &&
+            Configs.Generic.EASY_PLACE_SP_HANDLING.getBooleanValue())
+        {
+            return Vec3d.ZERO;
+        }
+
+        return hitVec.subtract(blockCenter);
+    }
+}
