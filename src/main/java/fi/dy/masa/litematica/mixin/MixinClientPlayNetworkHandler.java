b48ba38e3a284d10995ac3c00aa246d54e326904
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2022-07-30 23:51:21 +0300

    Fix the renderer not getting updated properly

--- src/main/java/fi/dy/masa/litematica/mixin/MixinClientPlayNetworkHandler.java
+++ src/main/java/fi/dy/masa/litematica/mixin/MixinClientPlayNetworkHandler.java
@@ -5,41 +5,27 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.network.packet.s2c.play.ChunkDataS2CPacket;
-import net.minecraft.network.packet.s2c.play.ChunkDeltaUpdateS2CPacket;
 import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
+import net.minecraft.network.packet.s2c.play.LightData;
 import net.minecraft.network.packet.s2c.play.UnloadChunkS2CPacket;
-import net.minecraft.util.math.ChunkSectionPos;
 import fi.dy.masa.litematica.config.Configs;
 import fi.dy.masa.litematica.data.DataManager;
-import fi.dy.masa.litematica.schematic.verifier.SchematicVerifier;
 import fi.dy.masa.litematica.util.SchematicWorldRefresher;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public abstract class MixinClientPlayNetworkHandler
 {
-    @Inject(method = "onChunkData", at = @At("RETURN"))
-    private void onChunkData(ChunkDataS2CPacket packetIn, CallbackInfo ci)
+    @Inject(method = "updateChunk", at = @At("HEAD"))
+    private void litematica_onUpdateChunk(int x, int z, LightData lightData, CallbackInfo ci)
     {
         if (Configs.Visuals.ENABLE_RENDERING.getBooleanValue() &&
             Configs.Visuals.ENABLE_SCHEMATIC_RENDERING.getBooleanValue())
         {
-            SchematicWorldRefresher.INSTANCE.markSchematicChunksForRenderUpdate(packetIn.getX(), packetIn.getZ());
+            SchematicWorldRefresher.INSTANCE.markSchematicChunksForRenderUpdate(x, z);
         }
 
-        DataManager.getSchematicPlacementManager().onClientChunkLoad(packetIn.getX(), packetIn.getZ());
-    }
-
-    @Inject(method = "onChunkDeltaUpdate", at = @At("RETURN"))
-    private void onChunkDelta(ChunkDeltaUpdateS2CPacket packet, CallbackInfo ci)
-    {
-        if (Configs.Visuals.ENABLE_RENDERING.getBooleanValue() &&
-            Configs.Visuals.ENABLE_SCHEMATIC_RENDERING.getBooleanValue())
-        {
-            ChunkSectionPos pos = ((IMixinChunkDeltaUpdateS2CPacket) packet).litematica_getSection();
-            SchematicWorldRefresher.INSTANCE.markSchematicChunksForRenderUpdate(pos.getX(), pos.getY(), pos.getZ());
-            packet.visitUpdates((p, s) -> SchematicVerifier.markVerifierBlockChanges(p));
-        }
+        DataManager.getSchematicPlacementManager().onClientChunkLoad(x, z);
+        // TODO verifier updates?
     }
 
     @Inject(method = "onUnloadChunk", at = @At("RETURN"))
