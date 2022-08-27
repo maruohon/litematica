8c12bc7cba9da5b11864badbc9f949f3ba766bf4
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2022-08-27 17:56:27 +0300

    Clean up some debug logging things

--- src/main/java/fi/dy/masa/litematica/mixin/MixinWorldRenderer.java
+++ src/main/java/fi/dy/masa/litematica/mixin/MixinWorldRenderer.java
@@ -1,15 +1,15 @@
 package fi.dy.masa.litematica.mixin;
 
-import net.minecraft.client.render.Camera;
-import net.minecraft.client.render.Frustum;
-import net.minecraft.util.math.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.Frustum;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.math.Matrix4f;
 import fi.dy.masa.litematica.render.LitematicaRenderer;
 
 @Mixin(net.minecraft.client.render.WorldRenderer.class)
