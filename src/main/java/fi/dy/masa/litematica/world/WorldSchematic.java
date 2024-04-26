e0bfbb1b6e798518e871189792aca63b0cf34dfd
Zach Jones <23038709+zachtjones@users.noreply.github.com>
2024-04-26 08:33:29 -0700

    Support for 1.20.5 snapshots and local malilib

--- src/main/java/fi/dy/masa/litematica/world/WorldSchematic.java
+++ src/main/java/fi/dy/masa/litematica/world/WorldSchematic.java
@@ -6,6 +6,7 @@ import java.util.function.Predicate;
 import java.util.function.Supplier;
 import com.google.common.collect.ImmutableList;
 import net.minecraft.component.type.MapIdComponent;
+import net.minecraft.recipe.BrewingRecipeRegistry;
 import org.jetbrains.annotations.Nullable;
 
 import net.minecraft.block.Block;
@@ -500,6 +501,11 @@ public class WorldSchematic extends World
     public DynamicRegistryManager getRegistryManager()
     {
         return this.mc.world.getRegistryManager();
+    }
+
+    @Override
+    public BrewingRecipeRegistry getBrewingRecipeRegistry() {
+        return this.mc.world.getBrewingRecipeRegistry();
     }
 
     @Override
