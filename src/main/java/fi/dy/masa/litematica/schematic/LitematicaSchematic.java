afb3feb258aacdc52901217bec233e2bee880b21
Sakura Ryoko <116967773+sakura-ryoko@users.noreply.github.com>
2025-02-28 13:07:57 -0500

    Sponge schematics: Don't require a `BlockEntities` tag to be present

--- src/main/java/fi/dy/masa/litematica/schematic/LitematicaSchematic.java
+++ src/main/java/fi/dy/masa/litematica/schematic/LitematicaSchematic.java
@@ -1394,8 +1394,7 @@ public class LitematicaSchematic
             blocksTag = tag.getCompound("Blocks");
 
             if (blocksTag.contains("Palette", Constants.NBT.TAG_COMPOUND) &&
-                blocksTag.contains("Data", Constants.NBT.TAG_BYTE_ARRAY) &&
-                blocksTag.contains("BlockEntities", Constants.NBT.TAG_LIST))
+                blocksTag.contains("Data", Constants.NBT.TAG_BYTE_ARRAY))
             {
                 paletteTag = blocksTag.getCompound("Palette");
                 blockData = blocksTag.getByteArray("Data");
@@ -1466,6 +1465,12 @@ public class LitematicaSchematic
     {
         Map<BlockPos, NbtCompound> blockEntities = new HashMap<>();
         String tagName = spongeVersion == 1 ? "TileEntities" : "BlockEntities";
+        
+        if (tag.contains(tagName) == false)
+        {
+            return blockEntities;
+        }
+        
         NbtList tagList = tag.getList(tagName, Constants.NBT.TAG_COMPOUND);
 
         final int size = tagList.size();
